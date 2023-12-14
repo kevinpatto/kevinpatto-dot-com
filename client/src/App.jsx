cea22ccac47a597226bc4500ba9f796cc4806c9d
Kevin Patto <kevp2010@gmail.com>
2023-12-14 12:39:19 -0600

    implemented apollogql

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,48 @@
 import { Outlet } from 'react-router-dom';
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import './App.css';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-function App() {
+const httpLink = createHttpLink({
+	uri: '/graphql',
+});
 
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem('id_token');
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
+});
+
+const client = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
+
+function App() {
 	return (
 		<>
-			<Header />
-			<main>
-				<Outlet />
-			</main>
-			<Footer />
+			<ApolloProvider client={client}>
+				<Header />
+				<main>
+					<Outlet />
+				</main>
+				<Footer />
+			</ApolloProvider>
 		</>
 	)
 }
 
-export default App
+export default App;
