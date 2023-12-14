dabf90d9234eefe29f9aeb01eecadc5787710e93
Kevin Patto <kevp2010@gmail.com>
2023-12-14 12:38:44 -0600

    basic footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,10 @@
 
 export default function Footer() {
 	return (
-		<footer>
-			
+		<footer className="d-flex justify-content-center py-2 mt-auto">
+			<span className="px-2">
+				kevinpatto.com
+			</span>
 		</footer>
 	);
 }
